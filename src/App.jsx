e0fa8297a75a1879c8a78fa72f37e8dbdbe4b23f
orszaczkyanna <orszaczky.anna@gmail.com>
2024-05-29 23:28:46 +0200

    TemperatureConversionApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 // import CheckedProperty from './01_CheckedProperty/CheckedProperty.jsx'
 // import Switch from './02_Switch/Switch.jsx'
-import NumberGuessingGame from './03_NumberGuessingGame/NumberGuessingGame.jsx'
+// import NumberGuessingGame from './03_NumberGuessingGame/NumberGuessingGame.jsx'
+import TemperatureConversion from './04_TemperatureConversion/TemperatureConversion.jsx'
 
 function App() {
   return (
     <>
       {/* <CheckedProperty /> */}
       {/* <Switch /> */}
-      <NumberGuessingGame />
+      {/* <NumberGuessingGame /> */}
+      <TemperatureConversion />
     </>
   )
 }
