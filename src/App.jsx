c1c02ae22a4e39bb552fe580de76395a9acaf980
orszaczkyanna <orszaczky.anna@gmail.com>
2024-05-29 17:00:23 +0200

    Switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import CheckedProperty from './01_CheckedProperty/CheckedProperty.jsx'
+// import CheckedProperty from './01_CheckedProperty/CheckedProperty.jsx'
+import Switch from './02_Switch/Switch.jsx'
 
 function App() {
   return (
     <>
-      <CheckedProperty />
+      {/* <CheckedProperty /> */}
+      <Switch />
     </>
   )
 }
